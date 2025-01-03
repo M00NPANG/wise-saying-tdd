f5ed0aca76132dc85bd44e73316bc605316be191
gwony <moonsky1124@naver.com>
2025-01-03 12:36:30 +0900

    추가된 테스트 케이스 및 리펙터링

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,59 @@
+import org.example.TestApp;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FirstTest {
 
+
     @Test
     void t1() {
+        int rst = 1;
+        assertThat(rst).isEqualTo(1);
     }
 
+    @Test
+    void t2() {
+        TestApp app = new TestApp();
+        app.run();
+
+        // aaa가 출력되는가?
+        // assertThat(result).isEqualTo("aaa");
+    }
+
+    @Test
+    void t3() {
+        // 테스트봇 선입력
+//        Scanner sc = new Scanner("종료\n");
+//
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(out));
+//
+//        TestApp app = new TestApp();
+//        app.run();
+
+        String out = TestBot.run("종료");
+
+        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+
+        // 출력값을 체크
+    }
+
+    @Test
+    @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
+    void t4() {
+        String out = TestBot.run("종료");
+
+        // 테스트 코드 작성이 어려움.
+        // assertJ 를 사용법을 잘 모름. -> GTP를 활용하자
+        assertThat(out.toString())
+                .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
+
+    }
 }
