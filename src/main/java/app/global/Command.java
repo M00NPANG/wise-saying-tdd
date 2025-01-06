daaa79b064602185678616327a4313d359ec0ee8
gwony <moonsky1124@naver.com>
2025-01-06 10:34:47 +0900

    command t2 r

--- src/main/java/app/global/Command.java
+++ src/main/java/app/global/Command.java
@@ -10,6 +10,8 @@ public class Command {
     String actionName;
 
     public Command(String cmd) {
+        // split에서 만약 자를게 없으면 자기 자신이 나옴. 그래서 배열의 0번째로 반환.
+        // 따라서 0번째에 값이 없을 수 없음
         String[] cmdBits = cmd.split("\\?");
         actionName = cmdBits[0];
     }
--- src/test/java/app/global/CommandTest.java
+++ src/test/java/app/global/CommandTest.java
@@ -21,4 +21,30 @@ public class CommandTest {
         assertThat(actionName).isEqualTo("목록");
     }
 
+    @Test
+    @DisplayName("action 네임을 얻어올 수 있다. 입력값이 삭제. ?가 없으면 잘 나오는지 확인.")
+    void t3() {
+        Command cmd = new Command("삭제");
+        String actionName = cmd.getActionName();
+
+        assertThat(actionName).isEqualTo("삭제");
+    }
+
+    @Test
+    @DisplayName("불완전한 입력이 들어왔을 때, 삭제?id1, 삭제?id?1")
+    void t4() {
+        Command cmd = new Command("삭제");
+        String actionName = cmd.getActionName();
+
+        assertThat(actionName).isEqualTo("삭제");
+    }
+
+    @Test
+    @DisplayName("입력값 - 삭제?id=1일 때, 파라미터를 달라고 하면 1이 나와야 한다.")
+    void t5() {
+        Command cmd = new Command("삭제?id=1");
+        int id = cmd.getParam();
+        assertThat(id).isEqualTo(1);
+    }
+
 }
